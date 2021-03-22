d9251d1db7055c5b77b23ff677d38b59d2f82de7
SUSHANT <70851543+SUSHANT731@users.noreply.github.com>
2021-03-23 01:59:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,20 +3,4 @@
 include_once ("login.html");
 
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>okay..............</title>
-</head>
-<body>
-	<h1>home rahul page</h1>
-	  <?php if(!isset($_SESSION['username'])): header("location: logout.php");?>
 
-    <?php else: ?>
-
-    <?php endif ?>
-
-    <?php echo "<h1> Welcome ".$_SESSION['username']." To Dashboard </h1>" ?>
-<h2><a href="logout.php">Logout</a></h2>
-</body>
-</html>
